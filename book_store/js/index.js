2e526933fe48c02e5dd8ef76418c11f8e60cf661
Ahmad Ghanem <109623987+15ahmad9@users.noreply.github.com>
2023-01-10 23:46:58 +0300

    Add files via upload

--- book_store/js/index.js
+++ book_store/js/index.js
@@ -1,85 +1,86 @@
 // $.get("https://www.googleapis.com/books/v1/volumes?q="+searchData, getBookData()});
 
 
-$(document).ready(function() {
-    var item, tile, author, publisher, bookLink, bookImg;
-    
-    var outputList = document.getElementById("list-output");
-    var bookUrl = "https://www.googleapis.com/books/v1/volumes?q=";
-    var apiKey = "key=AIzaSyDtXC7kb6a7xKJdm_Le6_BYoY5biz6s8Lw";
-    var placeHldr = '<img src="https://via.placeholder.com/150">';
-    var searchData;
-    // outputList.innerHTML = `<h3 class="msg">Please Enter A Book Name</h3>`;
+$(document).ready(function () {
+  var item, searchData;
+  var outputList = document.getElementById("list-output");
+  var bookUrl = "https://www.googleapis.com/books/v1/volumes?q=";
+  var apiKey = "key=AIzaSyDtXC7kb6a7xKJdm_Le6_BYoY5biz6s8Lw";
+  var placeHldr = '<img src="https://via.placeholder.com/150">';
+  // outputList.innerHTML = `<h3 class="msg">Please Enter A Book Name</h3>`;
 
-    $("#search").click(function() {
-      
-      outputList.innerHTML = ""; //empty html output
-      document.body.style.backgroundImage = "url('')";
-       searchData = $("#book-name").val();
-       //handling empty search input field
-       if(searchData === "" || searchData === null) {
-         displayError();
-       }
-      else {
-         console.log(searchData);
-         
-         $.ajax({
-            url: bookUrl + searchData,
-            dataType: "json",
-            success: function(response) {
-              console.log(response)
-              if (response.totalItems === 0) {
-                alert("no result!.. try again")
-              }
-              else {
-                $("#title").animate({'margin-top': '5px'}, 1000); 
-                $("#list-output").css("visibility", "visible");
-                displayResults(response);
-              }
-            },
-            error: function () {
-              alert("Something went wrong.. <br>"+"Try again!");
-            }
-          });
+  $("#search").click(function () {
+
+    //empty html output
+    outputList.innerHTML = "";
+    document.body.style.backgroundImage = "url('')";
+    searchData = $("#book-name").val();
+    //handling empty search input field
+    if (searchData == "" || searchData == null) {
+      displayError();
+    }
+    else {
+      console.log(searchData);
+
+      $.ajax({
+        url: bookUrl + searchData,
+        dataType: "json",
+        success: function (response) {
+          console.log(response)
+          if (response.totalItems == 0) {
+            alert("no result!... try again")
+          }
+          else {
+            $("#title").animate({ 'margin-top': '5px' }, 1000);
+            $("#list-output").css("visibility", "visible");
+            displayResults(response);
+          }
+        },
+        error: function () {
+          alert("Something went wrong... <br>" + "Try again!");
         }
-        //مسح صندوق البحث
-        $("#book-name").val(""); 
-     });
-  
-     function displayResults(response) {
-        for (var i = 0; i < response.items.length; i+=2) {
-          item = response.items[i];
-          title1 = item.volumeInfo.title;
-          author1 = item.volumeInfo.authors;
-          publisher1 = item.volumeInfo.publisher;
-          bookLink1 = item.volumeInfo.previewLink;
-          previewLink = item.volumeInfo.industryIdentifiers[1].identifier
-          bookImg1 = (item.volumeInfo.imageLinks) ? item.volumeInfo.imageLinks.thumbnail : placeHldr ;
-  
-          item2 = response.items[i+1];
-          title2 = item2.volumeInfo.title;
-          author2 = item2.volumeInfo.authors;
-          publisher2 = item2.volumeInfo.publisher;
-          bookLink2 = item2.volumeInfo.previewLink;
-          previewLink = item2.volumeInfo.industryIdentifiers[1].identifier
-          bookImg2 = (item2.volumeInfo.imageLinks) ? item2.volumeInfo.imageLinks.thumbnail : placeHldr ;
-  
-          outputList.innerHTML += '<div class="row mt-4">' +
-          formatOutput(bookImg1, title1, author1, publisher1, bookLink1, previewLink) +
-          '</div>';
+      });
+    }
+    //مسح صندوق البحث
+    $("#book-name").val("");
+  });
 
-          outputList.innerHTML += '<div class="row mt-4">' +
-          formatOutput(bookImg2, title2, author2, publisher2, bookLink2, previewLink) +
-          '</div>';
-          
-          console.log(outputList);
-        }
-     }
+  function displayResults(response) {
+    for (var i = 0; i < response.items.length; i += 2) {
+      item = response.items[i];
+      title1 = item.volumeInfo.title;
+      author1 = item.volumeInfo.authors;
+      publisher1 = item.volumeInfo.publisher;
+      publishedDate1 = item.volumeInfo.publishedDate;
+      bookLink1 = item.volumeInfo.previewLink;
+      previewLink = item.volumeInfo.industryIdentifiers[1].identifier
+      bookImg1 = (item.volumeInfo.imageLinks) ? item.volumeInfo.imageLinks.thumbnail : placeHldr;
 
-     function formatOutput(bookImg, title, author, publisher, bookLink, previewLink) {
-      // console.log(title + ""+ author +" "+ publisher +" "+ bookLink+" "+ bookImg)
-      var viewUrl = 'https://books.google.jo/books?id='+previewLink +`&hl=&cd=4&source=gbs_api`; //constructing link for bookviewer
-      var htmlCard = `<div class="col-lg-6">
+      item2 = response.items[i + 1];
+      title2 = item2.volumeInfo.title;
+      author2 = item2.volumeInfo.authors;
+      publisher2 = item2.volumeInfo.publisher;
+      publishedDate2 = item.volumeInfo.publishedDate;
+      bookLink2 = item2.volumeInfo.previewLink;
+      previewLink = item2.volumeInfo.industryIdentifiers[1].identifier
+      bookImg2 = (item2.volumeInfo.imageLinks) ? item2.volumeInfo.imageLinks.thumbnail : placeHldr;
+
+      outputList.innerHTML += '<div class="row mt-4">' +
+        formatOutput(bookImg1, title1, author1, publisher1, publishedDate1, bookLink1, previewLink) +
+        '</div>';
+
+      outputList.innerHTML += '<div class="row mt-4">' +
+        formatOutput(bookImg2, title2, author2, publisher2, publishedDate2, bookLink2, previewLink) +
+        '</div>';
+
+      console.log(outputList);
+    }
+  }
+
+  function formatOutput(bookImg, title, author, publisher, publishedDate, bookLink, previewLink) {
+    // console.log(title + ""+ author +" "+ publisher +" "+ bookLink+" "+ bookImg)
+    var viewUrl = 'https://books.google.jo/books?id=' + previewLink + `&hl=&cd=4&source=gbs_api`; //constructing link for bookviewer
+    var Card = `<div class="col-lg-6">
         <div class="card" style="">
           <div class="row no-gutters">
             <div class="col-md-4" style="padding-top: 15px;">
@@ -91,19 +92,20 @@ $(document).ready(function() {
                 <h2 class="card-title">${title}</h2>
                 <p class="card-text">Author: ${author}</p>
                 <p class="card-text">Publisher: ${publisher}</p>
+                <p class="card-text">publishedDate: ${publishedDate}</p>
                 <a target="_blank" href="${viewUrl}" class="btn btn-secondary">Read Book</a>
               </div>
             </div>
           </div>
         </div>
       </div>`
-      return htmlCard;
-    }
+    return Card;
+  }
 
-    function displayError() {
-      alert("search term can not be empty!")
-    }
- 
- });
+  function displayError() {
+    alert("Search term can not be empty!")
+  }
+
+});
 
 
